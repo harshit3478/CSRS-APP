44dc293cd06e83001fdaea45d85e9bf39b1f7420
Harry <harsagra3478@gmail.com>
2025-01-15 17:55:16 +0530

    added postcss.config file

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: [
+      require('tailwindcss')(),
+      require('autoprefixer')(),
+      // If you're using any custom PostCSS plugins, make sure they're added here as well.
+    ],
+  };
+  
